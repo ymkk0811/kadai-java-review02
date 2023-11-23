8698e65aa2a8bbbc13f7f926fbf5f36560b55948
ymkk0811 <ymkk.hana@docomo.ne.jp>
2023-11-23 18:23:58 +0900

    コメントに課題提出と入力

--- src/Review02.java
+++ src/Review02.java
@@ -2,7 +2,7 @@
 public class Review02 {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        // 課題提出
         for(int i=1;i<=100;i++) {
             if(i%3==0&&i%5==0) {
                 System.out.println("FizzBuzz");
